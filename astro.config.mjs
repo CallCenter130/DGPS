25ec2578cb67a47629ea8833d8e39765fe0c5a35
CallCenter130 <callcenter@mtps.gob.sv>
2025-08-29 16:52:59 -0600

    build: change for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-import { Code } from 'astro:components';
 
 // https://astro.build/config
 export default defineConfig({
