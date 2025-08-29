af0720ce1cf6d77be896488cca708d7dc6053dc0
CallCenter130 <callcenter@mtps.gob.sv>
2025-08-29 16:37:33 -0600

    build: change for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
+code
+JavaScript
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  // Añade la URL completa de tu sitio de GitHub Pages
-  site: 'https://callcenter130.github.io',
+  // El 'site' es el dominio raíz.
+  site: 'https://callcenter130.github.io', 
   
-  // La base condicional que ya teníamos
+  // La 'base' condicional. Esta es la clave para que los enlaces internos funcionen.
   base: process.env.NODE_ENV === 'production' ? '/DGPS/' : '/',
 
   integrations: [tailwind(), alpinejs()]
